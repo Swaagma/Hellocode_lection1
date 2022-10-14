53a70de482e82e3b089fd06b1e1ccd123aa25e75
John Doe <Define2020@mail.ru>
2022-10-14 19:43:42 +0300

    Base 006

---
+++ Examples005_ifElse/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+
+{
+    Console.WriteLine("Господя, это же Машка! :)");
+}
+
+else
+
+{
+    Console.Write("Превед, Кревед ");
+    Console.WriteLine(username);
+}
